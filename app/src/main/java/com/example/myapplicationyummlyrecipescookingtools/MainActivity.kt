513822ef5976ef241fdb6ed95c8451fa2027f9e3
Reeta Thakur <thakurreeta@gmail.com>
2021-09-30 12:39:51 +0530

    all files commite

--- app/src/main/java/com/example/myapplicationyummlyrecipescookingtools/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationyummlyrecipescookingtools/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.myapplicationyummlyrecipescookingtools
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.one_extra_layout.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewMoreRelated.setOnClickListener {
+         val intent= Intent(this,RelatedActivity::class.java)
+            startActivity(intent)
+        }
+        viewMorePipEbby.setOnClickListener {
+            val intent= Intent(this,RelatedActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/myapplicationyummlyrecipescookingtools/Receipe.kt
@@ -0,0 +1,11 @@
+package com.example.myapplicationyummlyrecipescookingtools
+
+data class Receipe(
+    val Calories: String,
+    val Collection: String,
+    val Images: String,
+    val Ingredients: String,
+    val Minutes: String,
+    val RecipeName: String,
+    val User: String
+)
